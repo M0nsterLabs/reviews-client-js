03f6becb22efc5f24955a46a9998c7d3d98be3cb
Viram <viram@templatemonster.me>
2017-02-22 15:31:07 +0200

    Added reviews and test reviews

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+import serialize from 'tm-serialize';
 
 /**
  * Reviews API JS client.
@@ -24,39 +25,93 @@ export default class Reviews {
   /**
    * Return review information for given identifiers.
    * @param params {Object} Reviews parameters
-   * @returns {Array}
+   * @returns {Object} <pre>{
+   * "currentPageIndex":1,
+   * "totalCount":11,
+   * "lastPageIndex":6,
+   * "items": [
+   *  {
+   *    "id":23,
+   *    "status":4,
+   *    "user_id":21543,
+   *    "user_name":"mice mice",
+   *    "title":"review title",
+   *    "content":"review content",
+   *    "score":5,
+   *    "template_id":58444,
+   *    "vote_up":0,
+   *    "vote_down":0,
+   *    "_links":{"self":{"href":"http://service-reviews.dev/api/v1/reviews/23"}}
+   *   }, ...]
+   *  }</pre>
    * @method Reviews#getReviews
    */
   async getReviews(params = {}) {
     params = {...params, ...{locale: this.locale}};
-    const response = await fetch(`${this.url}/reviews?${this._serialize(params)}`);
+    const response = await fetch(`${this.url}/reviews?${serialize(params)}`);
     if (response.status >= 400) {
       throw new Error('Bad server response');
     }
-    return await response.json();
+    const paginationData   = {
+      currentPageIndex: parseInt(response.headers.get('x-pagination-current-page')),
+      totalCount: parseInt(response.headers.get('x-pagination-total-count')),
+      lastPageIndex: parseInt(response.headers.get('x-pagination-page-count'))
+    };
+
+    return {
+      ...paginationData,
+      items: await response.json()
+    };
   };
 
   /**
    * Return review information for given identifiers.
    * @param token {string} User token
    * @param params {Object} Reviews parameters
-   * @returns {Array}
+   * @returns {Object} <pre>{
+   * "currentPageIndex":1,
+   * "totalCount":11,
+   * "lastPageIndex":6,
+   * "items": [
+   *  {
+   *    "id":23,
+   *    "status":4,
+   *    "user_id":21543,
+   *    "user_name":"mice mice",
+   *    "title":"review title",
+   *    "content":"review content",
+   *    "score":5,
+   *    "template_id":58444,
+   *    "vote_up":0,
+   *    "vote_down":0,
+   *    "_links":{"self":{"href":"http://service-reviews.dev/api/v1/reviews/23"}}
+   *   }, ...]
+   *  }</pre>
    * @method Reviews#getReviewsClient
    */
-  getReviewsClient = (token, params = {}) => { return false; };
+  async getReviewsClient (token, params = {}) {
+    params = {...params, ...{locale: this.locale}};
+    if (!token.length) {
+      throw new Error('Token not found');
+    }
+    const response = await fetch(`${this.url}/reviews/client?${serialize(params)}`,{
+      headers: new Headers({
+        'Authorization' : token
+      })
+    });
+    console.log('response', response);
+    if (response.status >= 400) {
+      throw new Error('Bad server response');
+    }
+    const paginationData   = {
+      currentPageIndex: parseInt(response.headers.get('x-pagination-current-page')),
+      totalCount: parseInt(response.headers.get('x-pagination-total-count')),
+      lastPageIndex: parseInt(response.headers.get('x-pagination-page-count'))
+    };
 
-  /**
-   * Convert a js object into a query string
-   * @param obj
-   * @returns {string}
-   * @private
-   */
-  _serialize(obj) {
-    let str = [];
-    for (let p in obj)
-      if (obj.hasOwnProperty(p)) {
-        str.push(`${encodeURIComponent(p)}=${encodeURIComponent(obj[p])}`);
-      }
-    return str.join("&");
-  }
+    return {
+      ...paginationData,
+      items: await response.json()
+    }
+  };
 }
